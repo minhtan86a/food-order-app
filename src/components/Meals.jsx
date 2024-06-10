07fa9775262460b65d68927e454319b6569ccc0d
TanNguyen <minhtan86a@gmail.com>
2024-06-10 09:47:54 +0700

    267 Meals component and get data of meals

---
+++ src/components/Meals.jsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+export default function Meals() {
+  const [loadedMeals, setLoadedMeals] = useState([]);
+
+  ////GET data - useEffect to get rid of infinite loop
+  useEffect(() => {
+    async function fetchMeals() {
+      const response = await fetch("http://localhost:3000/meals");
+
+      if (!response.ok) {
+        //...
+      }
+
+      const meals = await response.json();
+      setLoadedMeals(meals);
+    }
+
+    fetchMeals();
+  }, []);
+
+  return (
+    <ul id="meals">
+      {loadedMeals.map((meal) => (
+        <li key={meal.id}>{meal.name}</li>
+      ))}
+    </ul>
+  );
+}
