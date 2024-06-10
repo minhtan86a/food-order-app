942f60ca432753d8099b26fa216198d8bdb8c598
TanNguyen <minhtan86a@gmail.com>
2024-06-10 20:11:48 +0700

    280 build Error component, show error

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,5 +1,6 @@
 import MealItem from "./MealItem";
 import useHttp from "../hooks/useHttp.js";
+import Error from "./Error.jsx";
 
 const requestConfig = {};
 
@@ -13,7 +14,11 @@ export default function Meals() {
   console.log(loadedMeals);
 
   if (isLoading) {
-    return <p>Fetching meals ...</p>;
+    return <p className="center">Fetching meals ...</p>;
+  }
+
+  if (error) {
+    return <Error title="Failed to fetch meals" message={error} />;
   }
 
   // if (!data) {
